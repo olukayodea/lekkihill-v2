e0845323e1d5bf72a22dc12414709dad04061d58
Olukayode <olukayode.adebiyi@hotmail.co.uk>
2023-03-13 22:14:53 -0400

    Update queries.sql

--- includes/queries.sql
+++ includes/queries.sql
@@ -2,3 +2,12 @@ ALTER TABLE `wp_lekkihill_appointments` ADD `create_by` INT NOT NULL AFTER `pati
 ALTER TABLE `wp_lekkihill_appointments` CHANGE `next_appointment` `next_appointment` DATETIME NULL;
 ALTER TABLE `wp_lekkihill_appointments` CHANGE `message` `message` VARCHAR(1000) CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
 ALTER TABLE `wp_lekkihill_inventory_category` ADD UNIQUE(`title`);
+ALTER TABLE `wp_lekkihill_invoice_log` CHANGE `amount` `amount` DOUBLE NOT NULL;
+ALTER TABLE `wp_lekkihill_clinic_medication` DROP `report_date`, DROP `report_time`;
+ALTER TABLE `wp_lekkihill_clinic_medication` ADD `doctors_report_id` INT NOT NULL AFTER `patient_id`;
+ALTER TABLE `wp_lekkihill_clinic_medication` ADD `invoice_id` INT NOT NULL AFTER `doctors_report_id`, ADD `quantity` INT NOT NULL AFTER `invoice_id`;
+DROP TABLE `lekkihill`.`wp_lekkihill_patient_medication`;
+ALTER TABLE `wp_lekkihill_clinic_fluid_balance` DROP `report_date`, DROP `report_time`;
+ALTER TABLE `wp_lekkihill_clinic_medication` ADD `inventory_id` INT NOT NULL AFTER `medication`;
+ALTER TABLE `wp_lekkihill_clinic_medication` ADD `sales_status` INT NOT NULL AFTER `frequency`;
+ALTER TABLE `wp_lekkihill_clinic_doctors_report` DROP `recommended`;
